9cfcbf8ea33dd8102003d4d1b8b46c8201487ce8
cwest9400 <chrstphrwest@gmail.com>
2022-12-07 11:53:15 -0500

    added Component: card detail & stylesheet(basic)

---
+++ src/components/CharacterDetail.js
@@ -0,0 +1,17 @@
+
+import '../styles/character-detail.css'
+
+export default function CardContainer(props) {
+    return (
+        <div className="character-detail">
+            <h2>Clazz name</h2>
+            <p>cool picture</p>
+            <p>Character Detail</p>
+            <p>character stats</p>
+            
+           
+        </div>
+
+    )
+
+}
