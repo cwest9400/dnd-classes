e5107ef75ba7a56fa8dbc9aedf24b60f1143d2d7
cwest9400 <chrstphrwest@gmail.com>
2022-12-07 13:56:16 -0500

    mapped api results to card container component

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -3,19 +3,26 @@ import { useState, useEffect } from "react"
 import '../styles/card-container.css'
 
 export default function CardContainer(props) {
-    const [clazz, setClazz] = useState ([])
+    const [clazzes, setClazzes] = useState ([])
     useEffect(() => {
         fetch("https://api.open5e.com/classes/")
             .then((res) => res.json())
             .then((json) => {
-                setClazz(json.results)
+                setClazzes(json.results)
             })
+            .catch(console.error)
     }, [])
-    console.log(clazz[1])
+    // console.log(clazzes[1])
     return (
         <div className="card-container">
-            <p> card container</p>
-            <CharacterCard />
+           {clazzes.map((clazz) => (
+               <div className="character-card">
+               <p>{clazz.name}</p>
+               <p>Cool picture</p>
+               
+           </div>
+
+           ))}
             
         </div>
 
