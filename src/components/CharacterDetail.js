4b66d1d6ddf4aa877d8b0d94351438bd6f9e4f24
cwest9400 <chrstphrwest@gmail.com>
2022-12-09 11:20:31 -0500

    added div containers to class detail component for grid structuring

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -21,29 +21,42 @@ export default function CharacterDetail(props) {
         return <p>Loading...</p>
     }
     return (
+        <>
+            <div className='class-detail-title'>
+                <h1>{name}</h1>
+            </div>
+            <div className="character-detail">
+                <img src={CLAZZ_IMAGES[name]} />
+                {/* <p>{clazzes.desc}</p> */}
+                <div className='class-stats'>
+                    <div className='stats'>
+                        <h2>Class stats</h2>
+                        <p>Hit Dice: {clazzes.hit_dice}</p>
+                        <p>Starting HP: {clazzes.hp_at_1st_level}</p>
+                        <p>Level up HP: {clazzes.hp_at_higher_levels}</p>
+                    </div>
+                    <div className='proficiencies'>
+                        <h3>Proficiencies</h3>
+                        <p>Armor: {clazzes.prof_armor}</p>
+                        <p>Weapon: {clazzes.prof_weapons}</p>
+                        <p>Tools: {clazzes.prof_tools}</p>
+                        <p>Saving throws: {clazzes.prof_saving_throws}</p>
+                    </div>
+                    <div className='skills'>
+                        <p>Skills: {clazzes.prof_skills}</p>
+                    </div>
+                    <div className='equipment'>
+                        <p>Equipment: {clazzes.equipment}</p>
+                    </div>
+                </div>
+                {/* <p>Stat Table {clazzes.table}</p> */}
+                <a href={dndBeyondUrl} target="_blank">
+                    Advanced description
+                </a>
 
-        <div className="character-detail">
-            <h1>{name}</h1>
-            <img src={CLAZZ_IMAGES[name]} />
-            {/* <p>{clazzes.desc}</p> */}
-            <h2>Class stats</h2>
-            <p>Hit Dice: {clazzes.hit_dice}</p>
-            <p>Starting HP: {clazzes.hp_at_1st_level}</p>
-            <p>Level up HP: {clazzes.hp_at_higher_levels}</p>
-            <h3>Proficiencies</h3>
-            <p>Armor: {clazzes.prof_armor}</p>
-            <p>Weapon: {clazzes.prof_weapons}</p>
-            <p>Tools: {clazzes.prof_tools}</p>
-            <p>Saving throws: {clazzes.prof_saving_throws}</p>
-            <p>Skills: {clazzes.prof_skills}</p>
-            <p>Equipment: {clazzes.equipment}</p>
-            {/* <p>Stat Table {clazzes.table}</p> */}
-            <a href={dndBeyondUrl} target="_blank">
-                Delve deeper
-            </a>
 
 
-
-        </div>
+            </div>
+        </>
     )
 }
